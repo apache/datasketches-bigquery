09a43a93e2ef6553ac1fc5cf5fa05be80b15f3bc
AlexanderSaydakov <AlexanderSaydakov@users.noreply.github.com>
2024-09-03 11:59:17 -0700

    configurable dataset

--- kll/test/kll_sketch_test.sql
+++ kll/test/kll_sketch_test.sql
@@ -17,26 +17,26 @@
  * under the License.
  */
 
-create or replace table t.kll_sketch(sketch bytes);
+create or replace table $BQ_DATASET.kll_sketch(sketch bytes);
 
-insert into t.kll_sketch
-(select t.kll_sketch_float_build(value, null) from unnest([1,2,3,4,5,6,7,8,9,10]) as value);
-insert into t.kll_sketch
-(select t.kll_sketch_float_build(value, null) from unnest([11,12,13,14,15,16,17,18,19,20]) as value);
+insert into $BQ_DATASET.kll_sketch
+(select $BQ_DATASET.kll_sketch_float_build(value, null) from unnest([1,2,3,4,5,6,7,8,9,10]) as value);
+insert into $BQ_DATASET.kll_sketch
+(select $BQ_DATASET.kll_sketch_float_build(value, null) from unnest([11,12,13,14,15,16,17,18,19,20]) as value);
 
-select t.kll_sketch_float_to_string(sketch) from t.kll_sketch;
+select $BQ_DATASET.kll_sketch_float_to_string(sketch) from $BQ_DATASET.kll_sketch;
 
 # expected 0.5
-select t.kll_sketch_float_get_rank(t.kll_sketch_float_merge(sketch, null), 10, true) from t.kll_sketch;
+select $BQ_DATASET.kll_sketch_float_get_rank($BQ_DATASET.kll_sketch_float_merge(sketch, null), 10, true) from $BQ_DATASET.kll_sketch;
 
 # expected 10
-select t.kll_sketch_float_get_quantile(t.kll_sketch_float_merge(sketch, null), 0.5, true) from t.kll_sketch;
+select $BQ_DATASET.kll_sketch_float_get_quantile($BQ_DATASET.kll_sketch_float_merge(sketch, null), 0.5, true) from $BQ_DATASET.kll_sketch;
 
 # expected 20
-select t.kll_sketch_float_get_n(t.kll_sketch_float_merge(sketch, null)) from t.kll_sketch;
+select $BQ_DATASET.kll_sketch_float_get_n($BQ_DATASET.kll_sketch_float_merge(sketch, null)) from $BQ_DATASET.kll_sketch;
 
 # expected 0.5, 0.5
-select t.kll_sketch_float_get_pmf(t.kll_sketch_float_merge(sketch, null), [10.0], true) from t.kll_sketch;
+select $BQ_DATASET.kll_sketch_float_get_pmf($BQ_DATASET.kll_sketch_float_merge(sketch, null), [10.0], true) from $BQ_DATASET.kll_sketch;
 
 # expected 0.5, 1
 select $BQ_DATASET.kll_sketch_float_get_cdf($BQ_DATASET.kll_sketch_float_merge(sketch, null), [10.0], true) from $BQ_DATASET.kll_sketch;
